03c1e1b74c453832ce69e55bf66e2165d09a17ef
MD. Tanvir <mt14050@gmail.com>
2023-04-12 22:25:30 +0600

    project done

--- script.js
+++ script.js
@@ -1,34 +1,24 @@
 $(document).ready(function () {
+    // create Dom
     $("#input__field").keypress(function (e) {
         let text = $("#input__field").val();
-
-        //create DOM
-        let list = document.createElement('li');
-        list.innerText = text
-
-        let Div = document.createElement('div');
-        Div.classList.add("button_box");
-
-        let spanRight = document.createElement('span');
-        let rightImg = document.createElement('img');
-        rightImg.src = "/images/check.png";
-        spanRight.appendChild(rightImg)
-
-        let spanDelete = document.createElement('span');
-        let deleteImg = document.createElement('img');
-        deleteImg.src = "/images/delete.png";
-        spanDelete.appendChild(deleteImg)
-
-        Div.appendChild(spanRight);
-        Div.appendChild(spanDelete);
-
-        list.appendChild(Div)
-        //
-
-        if (e.which == 13) {
-            $('ul').append(list);
+        if (e.which == 13 && text.length > 0) {
+            $('ul').append('<li>'+ text +'<div class="button_box"><span><img class="check" src="/images/check.png" alt="check"></span><span> <img class="delete" src="/images/delete.png" alt="delete"></span> </div>' + '</li>');
             $("#input__field").val('')
         }
     })
-    
+    $("#submit").click(function () {
+        let text = $("#input__field").val();
+        if (text.length > 0) {
+            $('ul').append('<li>' + text + '<div class="button_box"><span><img class="check" src="/images/check.png" alt="check"></span><span> <img class="delete" src="/images/delete.png" alt="delete"></span> </div>' + '</li>');
+            $("#input__field").val('')
+        }    
+    })
+    // delete Dom
+    $("ul").on('click', '.delete', function () {
+        $(this).parents("li").remove();
+    })
+    $("ul").on('click', '.check', function () {
+        $(this).parents("li").css({ 'text-decoration': 'line-through', 'backgroundColor':'#90EE90'})
+    })
 })
